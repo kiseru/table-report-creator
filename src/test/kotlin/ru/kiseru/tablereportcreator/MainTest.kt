087667f8ce1054a8fcd797901b671aead5a9e0d6
Alexandr Kiselev <kis.alex97@gmail.com>
2025-09-29 11:55:25 +0300

    added supporting outputStream

--- src/test/kotlin/ru/kiseru/tablereportcreator/MainTest.kt
+++ src/test/kotlin/ru/kiseru/tablereportcreator/MainTest.kt
@@ -3,27 +3,34 @@ package ru.kiseru.tablereportcreator
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
 
 
 class MainTest {
 
     @Test
     fun `test setting title`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when
-        val report = createReport {
+        createReport(outputStream) {
             setTitle("Some cool title")
         }
 
         // then
-        assertThat(report).isEqualTo(" Some cool title\n")
+        assertThat(outputStream.toString()).isEqualTo(" Some cool title\n")
     }
 
     @Test
     fun `test setting title twice`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when & then
         assertThatExceptionOfType(IllegalStateException::class.java)
             .isThrownBy {
-                createReport {
+                createReport(outputStream) {
                     setTitle("Some cool title")
                     setTitle("Another cool title")
                 }
@@ -33,10 +40,13 @@ class MainTest {
 
     @Test
     fun `test setting title after creating table`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when & then
         assertThatExceptionOfType(IllegalStateException::class.java)
             .isThrownBy {
-                createReport {
+                createReport(outputStream) {
                     createTable(0) {}
                     setTitle("Some cool title")
                 }
@@ -46,21 +56,27 @@ class MainTest {
 
     @Test
     fun `test setting table title without report title`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when
-        val result = createReport {
+        createReport(outputStream) {
             createTable(0, 0) {
                 setTitle("Some cool table title")
             }
         }
 
         // then
-        assertThat(result).isEqualTo(" Some cool table title\n")
+        assertThat(outputStream.toString()).isEqualTo(" Some cool table title\n")
     }
 
     @Test
     fun `test setting table title with report title`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when
-        val result = createReport {
+        createReport(outputStream) {
             setTitle("Some cool report title")
 
             createTable(0, 0) {
@@ -69,15 +85,18 @@ class MainTest {
         }
 
         // then
-        assertThat(result).isEqualTo(" Some cool report title\n\n Some cool table title\n")
+        assertThat(outputStream.toString()).isEqualTo(" Some cool report title\n\n Some cool table title\n")
     }
 
     @Test
     fun `test setting table title twice`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when & then
         assertThatExceptionOfType(IllegalStateException::class.java)
             .isThrownBy {
-                createReport {
+                createReport(outputStream) {
                     createTable(0, 0) {
                         setTitle("Some cool table title")
                         setTitle("Another cool table title")
@@ -89,10 +108,13 @@ class MainTest {
 
     @Test
     fun `test setting table title after setting table header`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when & then
         assertThatExceptionOfType(IllegalStateException::class.java)
             .isThrownBy {
-                createReport {
+                createReport(outputStream) {
                     createTable(3, 3) {
                         setHeaders("A", "B")
                         setTitle("Some cool table title")
@@ -104,10 +126,13 @@ class MainTest {
 
     @Test
     fun `test setting table title after setting data`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when & then
         assertThatExceptionOfType(IllegalStateException::class.java)
             .isThrownBy {
-                createReport {
+                createReport(outputStream) {
                     createTable(3, 3) {
                         data(
                             listOf("A", "B"),
@@ -121,15 +146,18 @@ class MainTest {
 
     @Test
     fun `test setting table headers`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when
-        val report = createReport {
+        createReport(outputStream) {
             createTable(3, 3) {
                 setHeaders("A", "B")
             }
         }
 
         // then
-        assertThat(report)
+        assertThat(outputStream.toString())
             .isEqualTo("""
                 ┌───┬───┐
                 │ A │ B │
@@ -139,26 +167,31 @@ class MainTest {
 
     @Test
     fun `test setting table headers twice`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when & then
         assertThatExceptionOfType(IllegalStateException::class.java)
             .isThrownBy {
-                val result = createReport {
+                createReport(outputStream) {
                     createTable(3, 3) {
                         setHeaders("A", "B")
                         setHeaders("A", "B")
                     }
                 }
-                println(result)
             }
             .withMessage("Table headers is already set")
     }
 
     @Test
     fun `test setting table headers after setting data`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when & then
         assertThatExceptionOfType(IllegalStateException::class.java)
             .isThrownBy {
-                val result = createReport {
+                createReport(outputStream) {
                     createTable(3, 3) {
                         data(
                             listOf("1", "2")
@@ -166,17 +199,19 @@ class MainTest {
                         setHeaders("A", "B")
                     }
                 }
-                println(result)
             }
             .withMessage("Table data is already set")
     }
 
     @Test
     fun `test setting table data twice`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when & then
         assertThatExceptionOfType(IllegalStateException::class.java)
             .isThrownBy {
-                val result = createReport {
+                createReport(outputStream) {
                     createTable(3, 3) {
                         data(
                             listOf("1", "2")
@@ -186,22 +221,24 @@ class MainTest {
                         )
                     }
                 }
-                println(result)
             }
             .withMessage("Table data is already set")
     }
 
     @Test
     fun `test setting table data without table headers`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when
-        val report = createReport {
+        createReport(outputStream) {
             createTable(3, 3) {
                 data(listOf("1", "2"))
             }
         }
 
         // then
-        assertThat(report)
+        assertThat(outputStream.toString())
             .isEqualTo("""
                 ┌───┬───┐
                 │ 1 │ 2 │
@@ -211,8 +248,11 @@ class MainTest {
 
     @Test
     fun `test setting table data with table headers`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when
-        val report = createReport {
+        createReport(outputStream) {
             createTable(3, 3) {
                 setHeaders("A", "B")
                 data(listOf("1", "2"))
@@ -220,7 +260,7 @@ class MainTest {
         }
 
         // then
-        assertThat(report)
+        assertThat(outputStream.toString())
             .isEqualTo("""
                 ┌───┬───┐
                 │ A │ B │
@@ -232,32 +272,37 @@ class MainTest {
 
     @Test
     fun `test setting table data more than columns exists`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when & then
         assertThatExceptionOfType(IllegalArgumentException::class.java)
             .isThrownBy {
-                val result = createReport {
+                createReport(outputStream) {
                     createTable(3, 3) {
                         data(
                             listOf("1", "2", "3")
                         )
                     }
                 }
-                println(result)
             }
             .withMessage("The table have 2 columns.")
     }
 
     @Test
     fun `test setting table data with value more than column size`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when
-        val report = createReport {
+        createReport(outputStream) {
             createTable(3, 3) {
                 data(listOf("12", "2"))
             }
         }
 
         // then
-        assertThat(report)
+        assertThat(outputStream.toString())
             .isEqualTo("""
                 ┌───┬───┐
                 │ 1 │ 2 │
@@ -267,15 +312,18 @@ class MainTest {
 
     @Test
     fun `test setting table data with value less than column size`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when
-        val report = createReport {
+        createReport(outputStream) {
             createTable(4, 3) {
                 data(listOf("1", "2"))
             }
         }
 
         // then
-        assertThat(report)
+        assertThat(outputStream.toString())
             .isEqualTo("""
                 ┌────┬───┐
                 │ 1  │ 2 │
@@ -285,30 +333,35 @@ class MainTest {
 
     @Test
     fun `test setting table headers more than columns exists`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when & then
         assertThatExceptionOfType(IllegalArgumentException::class.java)
             .isThrownBy {
-                val result = createReport {
+                createReport(outputStream) {
                     createTable(3, 3) {
                         setHeaders("A", "B", "C")
                     }
                 }
-                println(result)
             }
             .withMessage("The table have 2 columns.")
     }
 
     @Test
     fun `test setting table headers #1`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when
-        val report = createReport {
+        createReport(outputStream) {
             createTable(5, 3) {
                 setHeaders("A", "B")
             }
         }
 
         // then
-        assertThat(report)
+        assertThat(outputStream.toString())
             .isEqualTo("""
                 ┌─────┬───┐
                 │  A  │ B │
@@ -318,15 +371,18 @@ class MainTest {
 
     @Test
     fun `test setting table headers #2`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when
-        val report = createReport {
+        createReport(outputStream) {
             createTable(4, 3) {
                 setHeaders("A", "B")
             }
         }
 
         // then
-        assertThat(report)
+        assertThat(outputStream.toString())
             .isEqualTo("""
                 ┌────┬───┐
                 │ A  │ B │
@@ -336,15 +392,18 @@ class MainTest {
 
     @Test
     fun `test setting table headers #3`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when
-        val report = createReport {
+        createReport(outputStream) {
             createTable(5, 3) {
                 setHeaders("AC", "B")
             }
         }
 
         // then
-        assertThat(report)
+        assertThat(outputStream.toString())
             .isEqualTo("""
                 ┌─────┬───┐
                 │ AC  │ B │
@@ -354,15 +413,18 @@ class MainTest {
 
     @Test
     fun `test setting table headers #4`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when
-        val report = createReport {
+        createReport(outputStream) {
             createTable(6, 3) {
                 setHeaders("AC", "B")
             }
         }
 
         // then
-        assertThat(report)
+        assertThat(outputStream.toString())
             .isEqualTo("""
                 ┌──────┬───┐
                 │  AC  │ B │
@@ -372,15 +434,18 @@ class MainTest {
 
     @Test
     fun `test setting table headers #5`() {
+        // given
+        val outputStream = ByteArrayOutputStream()
+
         // when
-        val report = createReport {
+        createReport(outputStream) {
             createTable(3, 3) {
                 setHeaders("AC", "B")
             }
         }
 
         // then
-        assertThat(report)
+        assertThat(outputStream.toString())
             .isEqualTo("""
                 ┌───┬───┐
                 │ A │ B │
