bd3ad7d6032be0c5526f4d210c92034de888b3a7
Alexandr Kiselev <kis.alex97@gmail.com>
2025-09-29 11:16:35 +0300

    added tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,15 @@ application {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.12.2")
+    testImplementation("org.assertj:assertj-core:3.27.4")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
     jvmToolchain(17)
+}
+
+tasks.withType<Test>() {
+    useJUnitPlatform()
 }
